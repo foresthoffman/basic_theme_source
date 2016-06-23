9939235dc3bc3a2032bdedd479e599f2484e26cf
Forest Hoffman <foresthoffman@gmail.com>
2016-06-23 10:16:05 -0700

    Made the non-front-page site-title an `h1` instead of `p` tag, made mobile layout changes

--- header.php
+++ header.php
@@ -26,14 +26,8 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
-
 			$description = get_bloginfo( 'description', 'display' );
 			if ( $description || is_customize_preview() ) : ?>
 				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
