80dd094def530f61efad0c0d1c42d4b40e60a25e
Forest Hoffman <foresthoffman@users.noreply.github.com>
2016-06-23 13:41:50 -0700

    Updated comment styles, mobile-sidebar layout spacing and sidebar template, and minor header and footer styles

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bt' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'bt' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'bt' ), 'bt', '<a href="http://forest.stfhs.net/forest" rel="designer">Forest Hoffman</a>' ); ?>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s', 'bt' ), 'bt', '<a href="http://forest.stfhs.net/forest" rel="designer">Forest Hoffman</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- sidebar.php
+++ sidebar.php
@@ -7,11 +7,92 @@
  * @package Basic_Theme
  */
 
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-	return;
+function bt_default_widget_args ( $widget_name ) {
+	return array(
+		'before_widget' => "<section class='widget {$widget_name}'>",
+		'after_widget'  => '</section>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	);
 }
+
+if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 ?>
 
-<aside id="secondary" class="widget-area" role="complementary">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
-</aside><!-- #secondary -->
+	<aside id="secondary" class="widget-area" role="complementary">
+		<?php
+
+		/*
+		 * Default Sidebar Widgets
+		 */
+
+		// Search Widget
+		the_widget(
+			'WP_Widget_Search',
+			array(
+				'title' => null
+			),
+			bt_default_widget_args( 'widget_search' )
+		);
+
+		// Recent Posts
+		the_widget(
+			'WP_Widget_Recent_Posts',
+			array(
+				'title' => __( 'Recent Posts', 'bt' )
+			),
+			bt_default_widget_args( 'widget_recent_entries' )
+		);
+
+		// Recent Comments
+		the_widget(
+			'WP_Widget_Recent_Comments',
+			array(
+				'title' => __( 'Recent Comments', 'bt' )
+			),
+			bt_default_widget_args( 'widget_recent_comments' )
+		);
+
+		// Tag Cloud
+		the_widget(
+			'WP_Widget_Tag_Cloud',
+			array(
+				'title' => __( 'Tags', 'bt' )
+			),
+			bt_default_widget_args( 'widget_tag_cloud' )
+		);
+
+		// Categories
+		the_widget(
+			'WP_Widget_Categories',
+			array(
+				'title' => __( 'Categories', 'bt' ),
+				'hierarchical' => 1,
+				'dropdown' => 1
+			),
+			bt_default_widget_args( 'widget_categories' )
+		);
+
+		// Meta
+		the_widget(
+			'WP_Widget_Meta',
+			array(
+				'title' => __( 'Links', 'bt' )
+			),
+			bt_default_widget_args( 'widget_meta' )
+		);
+
+		?>
+	</aside><!-- #secondary -->
+
+<?php
+} else {
+?>
+
+	<aside id="secondary" class="widget-area" role="complementary">
+		<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	</aside><!-- #secondary -->
+
+<?php
+}
+?>
