99bf2c7e42f319df8c811de24cb8c467ac0815b1
Forest Hoffman <foresthoffman@users.noreply.github.com>
2016-06-24 20:35:19 -0700

    Moved scripts from base js/ directory to js/src/ to be built into js/dist/

--- inc/customizer.php
+++ inc/customizer.php
@@ -21,6 +21,6 @@ add_action( 'customize_register', 'bt_customize_register' );
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function bt_customize_preview_js() {
-	wp_enqueue_script( 'bt_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+	wp_enqueue_script( 'bt_customizer', get_template_directory_uri() . '/js/dist/customizer.min.js', array( 'customize-preview' ), '20151215', true );
 }
 add_action( 'customize_preview_init', 'bt_customize_preview_js' );
