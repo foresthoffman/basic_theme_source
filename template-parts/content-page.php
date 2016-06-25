6e0024773925b22956014942215cc97a4ac8634d
Forest Hoffman <foresthoffman@gmail.com>
2016-06-24 19:20:29 -0700

    Updated header styles, desktop-resolution nav styles, and added permalink to page headers

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -11,7 +11,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php the_title( '<h1 class="entry-title"><a class="bt_permlink" href="' . esc_url( get_permalink() ) . '" rel="bookmark">&para;</a> ', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
